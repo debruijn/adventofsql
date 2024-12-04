38ec951d1f5c27688c0169c565700d8e153710bf
Bert de Bruijn <lpdebruijn@gmail.com>
2024-12-04 10:47:08 -0400

    Update template to what I actually use as template each day

--- template/main.sql
+++ template/main.sql
@@ -1,29 +1,5 @@
--- Template queries to overwrite for specific days
+-- Make sure to run `advent_of_sql_day_3.sql` first, which you need to get from the website
+-- My approach is to step by step show how I have build up the queries, but only the last one really matters
 
 
-CREATE TABLE temp (
-                         temp_char		varchar(80),
-                         temp_int		int
-);
-
-CREATE TABLE temp2 (
-                       temp_char		varchar(80),
-                       temp_date	    date
-);
-
-INSERT INTO temp (temp_char, temp_int)
-VALUES ('Case 1', 43);
-
-INSERT INTO temp (temp_char, temp_int)
-VALUES ('Case 2', 44);
-
-INSERT INTO temp2 (temp_date, temp_char)
-VALUES ('1994-11-29', 'Case 1');
-
-INSERT INTO temp2 (temp_date, temp_char)
-VALUES ('1994-11-30', 'Case 2');
-
-SELECT temp_int, temp_date
-FROM temp JOIN temp2 ON temp.temp_char = temp2.temp_char;
-
-DROP TABLE temp, temp2;
+-- Plan:
