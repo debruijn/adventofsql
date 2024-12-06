83ed77e497fe04340dc48877764f1da06c884e9b
Bert de Bruijn <lpdebruijn@gmail.com>
2024-12-06 18:29:01 -0400

    Change accidental referral to day 3 in the template and days 4/5/6

--- 2024/05/main.sql
+++ 2024/05/main.sql
@@ -1,4 +1,4 @@
--- Make sure to run `advent_of_sql_day_3.sql` first, which you need to get from the website
+-- Make sure to run `advent_of_sql_day_5.sql` first, which you need to get from the website
 -- My approach is to step by step show how I have build up the queries, but only the last one really matters
 
 
